1400b5c497e3d618280e703dc8150ae8c59a78d9
whitneypurdum <whitney.purdum@energyweb.org>
2022-06-14 19:19:41 +0100

    fix: apply prettier formatting

--- packages/credential-governance/src/types/role-credential.ts
+++ packages/credential-governance/src/types/role-credential.ts
@@ -1,21 +1,19 @@
-import {
-    CredentialSubject,
-  } from '@ew-did-registry/credentials-interface';
+import { CredentialSubject } from '@ew-did-registry/credentials-interface';
 
 export interface IssuerFields {
-    key: string;
-    value: string | number;
-  }
-  
-  export interface RoleCredentialSubject extends CredentialSubject {
-    /*
-     * https://www.w3.org/TR/vc-data-model/#identifiers
-     */
-    id: string;
-  
-    role: {
-      namespace: string;
-      version: string;
-    };
-    issuerFields: IssuerFields[];
-  }
+  key: string;
+  value: string | number;
+}
+
+export interface RoleCredentialSubject extends CredentialSubject {
+  /*
+   * https://www.w3.org/TR/vc-data-model/#identifiers
+   */
+  id: string;
+
+  role: {
+    namespace: string;
+    version: string;
+  };
+  issuerFields: IssuerFields[];
+}
